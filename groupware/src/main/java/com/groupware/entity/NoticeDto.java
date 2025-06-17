b2599904af92a4fb8870033a8b438c356088e881
김장호 <gimj07182@gmial.com>
2025-06-17 16:17:55 +0900

    commit to notice dto

---
+++ groupware/src/main/java/com/groupware/entity/NoticeDto.java
@@ -0,0 +1,50 @@
+package com.groupware.entity;
+
+import java.sql.Timestamp;
+import org.hibernate.annotations.CreationTimestamp;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="notice")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class NoticeDto {
+	
+	@Id //기본키 설정 (고유번호)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int   	  ntcno;
+	
+	// 비밀번호
+	@Column(nullable = false, length = 200)
+	private String    ntcps;
+	
+	// 제목
+	@Column(nullable = false, length = 200)
+	private String    ntctt;
+	
+	// 작성자(관리자)
+	@Column(nullable = false, length = 200)
+	private String    ntcwr;
+	
+	// 게시일
+	@CreationTimestamp
+	private Timestamp ntcrd;
+	
+	// 내용
+	@Column(length = 5000)
+	private String    ntcct;
+	
+	// 조회수
+	private int ntcht = 0;
+}
