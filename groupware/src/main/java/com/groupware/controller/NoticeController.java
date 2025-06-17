46a580176d15446a2b3082841df9a1b476cff312
김장호 <gimj07182@gmial.com>
2025-06-17 16:24:22 +0900

    commit to noticecontroller

---
+++ groupware/src/main/java/com/groupware/controller/NoticeController.java
@@ -0,0 +1,16 @@
+package com.groupware.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.jpaproject.service.NoticeService;
+
+@RestController
+@RequestMapping("/notice")
+public class NoticeController {
+	
+	// 서비스 설정
+	private final NoticeService noticeService;
+	
+	// 컨트롤러 서비스 실행설정
+	public NoticeController(NoticeService noticeService) {this.noticeService = noticeService;}
+}
