06747736f952ac78a26f36e9b11066b9a7704872
김장호 <gimj07182@gmial.com>
2025-06-17 17:20:30 +0900

    commit to repository

--- groupware/src/main/java/com/groupware/repository/NoticeRepository.java
+++ groupware/src/main/java/com/groupware/repository/NoticeRepository.java
@@ -3,4 +3,6 @@ package com.groupware.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.groupware.entity.NoticeDto;
 
-public interface NoticeRepository extends JpaRepository<NoticeDto,Integer> {}
+public interface NoticeRepository extends JpaRepository<NoticeDto,Integer> {
+	
+}
