2001a61cd86c483114ee6d51186962408d77945e
김장호 <gimj07182@gmial.com>
2025-06-17 17:54:34 +0900

    Merge branch 'main' of https://github.com/soob0519/groupwareProject.git into main

---
+++ groupware/src/main/java/com/groupware/search/SearchDto.java
@@ -0,0 +1,12 @@
+package com.groupware.search;
+
+import lombok.Data;
+
+@Data
+public class SearchDto {
+
+	String search;
+	
+	public String getSearch() {return search;}
+	public void setSearch(String search) {this.search = search;}
+}
