9ec86580f929758d11c95684865034b103a8a4cb
김장호 <gimj07182@gmial.com>
2025-06-21 18:53:24 +0900

    commit to noticerepository

--- groupware/src/main/java/com/groupware/repository/NoticeRepository.java
+++ groupware/src/main/java/com/groupware/repository/NoticeRepository.java
@@ -1,8 +1,18 @@
 package com.groupware.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.groupware.entity.NoticeDto;
 
 public interface NoticeRepository extends JpaRepository<NoticeDto,Integer> {
 	
+	@Query(value = "select * from notice where ntctt like '%' || :search || '%' ",nativeQuery = true)
+	List<NoticeDto> searchByTitle(@Param("search") String search);
+	
+	Page<NoticeDto> findByNtcttContaining(String ntctt, Pageable pageable);
 }
