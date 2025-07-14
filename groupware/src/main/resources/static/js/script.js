610bcd9088e7046a5a7cecc50878717043a387cb
uyooyou <cjftjd299@naver.com>
2025-07-14 15:16:22 +0900

    CS html up

--- groupware/src/main/resources/static/js/script.js
+++ groupware/src/main/resources/static/js/script.js
@@ -1,4 +1,4 @@
-$(function () {
+$(document).ready(function () {
   $(".main > li > a").click(function (e) {
     e.preventDefault(); // a 태그 기본 동작 막기
 
@@ -18,5 +18,38 @@ $(function () {
       $submenu.slideDown(200);
       $arrow.addClass("rotate");
     }
+  });
+  
+  
+  // 모달 열기
+  $('.date_click').click(function () {
+      $('.modal_schedule_write').show();
+      $('#modal_1').css({
+          top: '80px',
+          left: '50%',
+          transform: 'translateX(-50%)'
+      });
+  });
+  $('#testbtn').click(function () {
+      $('.modal_sd_detail').show();
+      $('#modal_2').css({
+          top: '80px',
+          left: '50%',
+          transform: 'translateX(-50%)'
+      });
+  });
+
+
+  // 모달 닫기
+  $('.close_modal').click(function () {
+      $('.modal_schedule_write, .modal_sd_detail').hide();
+  });
+
+  // 드래그 설정 (transform 제거)
+  $('#modal_1, #modal_2').draggable({
+      handle: '.modal_header',
+      start: function () {
+          $(this).css('transform', 'none');
+      }
   });
 });
