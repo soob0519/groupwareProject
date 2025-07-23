88967b206b55c44aeb2001fc9f57350822d77e61
조수빈 <sbeen0519@gmail.com>
2025-07-23 10:51:13 +0900

    html 수정

--- groupware/src/main/java/com/groupware/repository/GnoticeRepository.java
+++ groupware/src/main/java/com/groupware/repository/GnoticeRepository.java
@@ -10,10 +10,13 @@ import com.groupware.entity.GnoticeDto;
 public interface GnoticeRepository extends JpaRepository<GnoticeDto,Integer> {
 	
 	// 일반공지사항 검색기능 (제목구분)
-	@Query(value = "select * from Gnotice where gntctt like '%' || :search || '%' ", nativeQuery = true)
+	@Query(value = "select * from Gnotice where gntctt like '%' || :search || '%' order by gntcrd desc ", nativeQuery = true)
 	
 	// 검색에 따른 데이터 개수에 따라 페이지 재 정렬
 	Page<GnoticeDto> findByGntcttContaining(@Param("search")String search, Pageable pageable);
+	
+	// 일반공지사항 분류기능 (부서구분)
+	@Query(value = "select * from Gnotice where deptno like %:deptno% order by gntcrd desc ", nativeQuery = true)
 	
 	// 부서분류에 따른 데이터 개수에 따라 페이지 재 정렬
 	Page<GnoticeDto> findByDeptnoContaining(String deptno, Pageable pageable);
--- groupware/src/main/java/com/groupware/repository/InoticeRepository.java
+++ groupware/src/main/java/com/groupware/repository/InoticeRepository.java
@@ -11,16 +11,21 @@ public interface InoticeRepository extends JpaRepository<InoticeDto,Integer> {
 	
 	// 전체공지사항 검색기능 + 필수공지사항 -> 일반공지사항 순으로 역순정렬 (제목구분)
 	@Query(value = " select * from Inotice where intctt like '%' || :search || '%' "
-				 + " order by case when intcca = '필수' then 0 else 1 end, intcno desc", 
+				 + " order by case when intcca = '필수' then 0 else 1 end, intcrd desc", 
 		   countQuery  = "  select count(*) from Inotice where intctt like '%' || :search || '%' ", nativeQuery = true)
 	
 	// 검색에 따른 데이터 개수에 따라 페이지 재 정렬
-	Page<InoticeDto> findByIntcttContaining(@Param("search")String search, Pageable pageable);
+	Page<InoticeDto> findByIntcttContaining(@Param("search")String search,Pageable pageable);
+	
+	// 전체공지사항 부서분류 + 필수공지사항 -> 일반공지사항 순으로 역순정렬 (제목구분)
+	@Query(value = " select * from Inotice where deptno like %:deptno% "
+				 + " order by case when intcca = '필수' then 0 else 1 end, intcrd desc ",
+		   countQuery = " select count(*) from inotice where deptno like %:deptno% ",nativeQuery = true)
+	
+	// 부서분류에 따른 데이터 개수에 따라 페이지 재 정렬
+	Page<InoticeDto> findByDeptnoContaining(String deptno, Pageable pageable);
 	
 	// 유형(필수) 값 개수 계산 SQL
 	@Query(value = " select count(*) from Inotice where intcca = '필수' ", nativeQuery = true)
 	long countByIntcca();
-	
-	// 부서분류에 따른 데이터 개수에 따라 페이지 재 정렬
-	Page<InoticeDto> findByDeptnoContaining(String deptno, Pageable pageable);
 }
